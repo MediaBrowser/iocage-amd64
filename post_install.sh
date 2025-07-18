f217b1007f746cbc1ca8f2308b0015d34a89eaf2
Maxime Gauduin <alucryd@archlinux.org>
2025-07-18 14:17:16 +0200

    Updates Emby server to version 4.9.0.70
    Upgrades dependency from mbedtls2 to mbedtls3 for improved security
    
    Updates binary package URL to pull latest stable release with security patches and feature improvements

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install the binary package
-pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.8.11.0/emby-server-freebsd14_4.8.11.0_amd64.pkg
+pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.9.0.70/emby-server-freebsd14_4.9.0.70_amd64.pkg
 
 # Enable the service
 sysrc -f /etc/rc.conf emby_server_enable="YES"
