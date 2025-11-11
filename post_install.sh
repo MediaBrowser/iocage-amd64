9ca265507c848cc6c7954619bc69b1da52b8025f
Luke <3607463+LukePulverenti@users.noreply.github.com>
2025-11-11 16:37:50 -0500

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install the binary package
-pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.9.0.70/emby-server-freebsd13_4.9.0.70_amd64.pkg
+pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.9.1.90/emby-server-freebsd13_4.9.1.90_amd64.pkg
 
 # Enable the service
 sysrc -f /etc/rc.conf emby_server_enable="YES"
